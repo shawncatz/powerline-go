46c7d5cb1f9c3423605f3e0019b749622adbd196
Shawn Catanzarite <me@shawncatz.com>
2017-02-03 15:03:59 -0800

    I think I prefer the bg / fg flipped back

--- powerline/theme.go
+++ powerline/theme.go
@@ -44,13 +44,13 @@ func SolarizedDark() Theme {
 func SolarizedDarkShawn() Theme {
 	return Theme{
 		ShellBg: "0",
-		Auth:    ColorPair{Bg: "0", Fg: "11"},
+		Auth:    ColorPair{Bg: "11", Fg: "0"},
 		Time:    ColorPair{Bg: "7", Fg: "0"},
 		Home:    ColorPair{Bg: "10", Fg: "0"},
 		Path:    ColorTriplet{Bg: "8", Fg: "0", SepFg: "0"},
 		Git: Git{
-			Clean: ColorPair{Bg: "0", Fg: "14"},
-			Dirty: ColorPair{Bg: "0", Fg: "10"},
+			Clean: ColorPair{Bg: "14", Fg: "0"},
+			Dirty: ColorPair{Bg: "10", Fg: "0"},
 		},
 		Lock:  ColorPair{Bg: "4", Fg: "7"},
 		Error: ColorPair{Bg: "1", Fg: "7"},
